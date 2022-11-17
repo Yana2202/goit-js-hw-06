21f5b1d600166e3958b5566dae5f120849fb4c76
Yana2202 <107585490+Yana2202@users.noreply.github.com>
2022-11-17 21:19:49 +0200

    update task 4

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,15 +1,14 @@
-const counter = {
-  countDecrement: document.querySelector('[data-action = "decrement"]'),
-  countIncrement: document.querySelector('[data-action = "increment"]'),
-  countValue: document.querySelector("#value"),
-};
-counter.countDecrement.addEventListener("click", onCountDecrement);
-counter.countIncrement.addEventListener("click", onCountIncrement);
-
 let counterValue = 0;
-function onCountDecrement() {
-  counter.counterValue.textContent = counterValue -= 1;
-}
-function onCountIncrement() {
-  counter.counterValue.textContent = counterValue += 1;
-}
+const countIncrement = document.querySelector('[data-action="increment"]');
+const countDecrement = document.querySelector('[data-action="decrement"]');
+const valueEL = document.querySelector("#value");
+
+countIncrement.addEventListener("click", () => {
+  counterValue += 1;
+  valueEL.textContent = counterValue;
+});
+
+countDecrement.addEventListener("click", () => {
+  counterValue -= 1;
+  valueEL.textContent = counterValue;
+});
