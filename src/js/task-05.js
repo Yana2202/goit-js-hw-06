f8eef5f82b7e3bac784ff658e0a9dd3f1dc42fb2
Yana2202 <107585490+Yana2202@users.noreply.github.com>
2022-11-18 18:11:53 +0200

    update task 5

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -1,8 +1,8 @@
-const textInput = document.querySelector("input#name-input");
-const output = document.querySelector("input#name-output");
+const textInput = document.querySelector("#name-input");
+const output = document.querySelector("#name-output");
 
 textInput.addEventListener("input", (event) => {
-  output.textContent = event.target.value;
+  output.textContent = event.currentTarget.value;
 
   if (event.target.value === "") {
     output.textContent = "Anonymous";
